520cc63be6fa54b02398dd05f50f8c749c5a5c7a
Roy Wei <roywjy@gmail.com>
2016-07-08 18:19:08 +0800

    make a status scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "statuses#index"
 end
