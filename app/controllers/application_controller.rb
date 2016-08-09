a6abf83f50af8cfe2702d79a0137b25524af1bc8
Roy Wei <roywjy@gmail.com>
2016-08-09 23:07:03 +0800

    no-bullets set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,39 +5,40 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:email,
-                         :password,
-                         :password_confirmation,
-                         :username,
-                         :image,
-                         :age,
-                         :gender,
-                         :location,
-                         :bio,
-                         :alt_email,
-                         :cover)
+       :password,
+       :password_confirmation,
+       :username,
+       :image,
+       :age,
+       :gender,
+       :location,
+       :bio,
+       :alt_email,
+       :cover)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:email,
-                         :password,
-                         :password_confirmation,
-                         :current_password,
-                         :username,
-                         :image,
-                         :age,
-                         :gender,
-                         :location,
-                         :bio,
-                         :alt_email,
-                         :cover
-                         :youtube_permission,
-                         :facebook_permission,
-                         :email_permission,
-                         :twitter_permission,
-                         :birthday_permission,
-                         :facebook,
-                         :twitter,
-                         :youtube)
+       :password,
+       :password_confirmation,
+       :current_password,
+       :username,
+       :image,
+       :cover,
+       :age,
+       :gender,
+       :location,
+       :bio,
+       :alt_email,
+       :youtube_permission,
+       :facebook_permission,
+       :email_permission,
+       :twitter_permission,
+       :birthday_permission,
+       :facebook,
+       :twitter,
+       :youtube
+       )
     end
   end
 end
